56ff138a5047c3ca756f4b5474b37f0ba1cc8ae9
alen <alen.dunn1@gmail.com>
2022-03-10 15:45:34 -0500

    add endpoints for index and notes

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:3001/ live on port ${PORT}.`);
+});
