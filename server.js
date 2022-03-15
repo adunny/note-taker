7da39e24e6250f41f705b00d23e1d581c146260f
alen <alen.dunn1@gmail.com>
2022-03-15 13:43:29 -0400

    move html endpoints to routes

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
-const path = require('path');
 
+// import routes
 const apiRoutes = require('./routes/apiRoutes');
-
-// npm package that generates a short random ID
-const shortid = require('shortid');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 
-
+// middleware
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
+
 app.use(express.static('public'));
 
+// endpoints
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-
-
-
-
-
-
+// server
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}/ is live!`);
 });
